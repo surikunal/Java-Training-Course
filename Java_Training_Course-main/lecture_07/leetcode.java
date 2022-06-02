08d8a7da4d1af008c032ee236cc5aa2aef94d539
surikunal <suri.kunal.222@gmail.com>
2022-06-02 12:03:58 +0530

    update

--- Java_Training_Course-main/lecture_07/leetcode.java
+++ Java_Training_Course-main/lecture_07/leetcode.java
@@ -54,4 +54,12 @@ public class leetcode {
     }
 
     // https://leetcode.com/problems/build-array-from-permutation/
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
 }
---
+++ Java_Training_Course-main/lecture_08/Main.java
@@ -0,0 +1,59 @@
+package lecture_08;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1,0,0,0,1,0,1},
+            {1,0,0,0,0,1,0},
+            {0,0,1,0,0,0,0},
+            {0,0,0,0,0,0,0},
+            {0,0,0,1,0,0,0},
+            {0,0,1,0,0,0,1}
+        };
+        exitPoint(arr);
+    }
+
+    public static void exitPoint(int[][] arr) {
+        // dir - 0 means right
+        // dir - 1 means down
+        // dir - 2 means left
+        // dir - 3 means up
+
+        int row = 0, col = 0;
+        int dir = 0;
+        while (true) {
+            dir = (dir + arr[row][col]) % 4;
+            // if (arr[row][col] == 1) {
+            //     dir = (dir + 1) % 4;
+            // } else {
+            //     dir = (dir + 0) % 4;
+            // }
+            if ( dir == 0 ) {
+                col++;
+                if (col == arr[0].length) {
+                    col--;
+                    break;
+                }
+            } else if ( dir == 1 ) {
+                row++;
+                if (row == arr.length) {
+                    row--;
+                    break;
+                }
+            } else if ( dir == 2 ) {
+                col--;
+                if (col == -1) {
+                    col++;
+                    break;
+                }
+            } else if ( dir == 3 ) {
+                row--;
+                if (row == -1) {
+                    row++;
+                    break;
+                }
+            }
+        }
+        System.out.println("(" + row + ", " + col + ")");
+    }
+}
